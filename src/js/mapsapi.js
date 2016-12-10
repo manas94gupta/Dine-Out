91b8dd53525e7f41ed2487290e6e38586e762882
Manas <manas94gupta@gmail.com>
2016-12-10 21:16:03 +0530

    Get users location using geolocation

--- src/js/mapsapi.js
+++ src/js/mapsapi.js
@@ -1,8 +1,11 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 28.570446, lng: 77.325854},
-    zoom: 14
+    center: {
+        lat: 20.593684,
+        lng: 78.96288
+    },
+    zoom: 6
   });
 
   // var atta = {lat: 28.572728, lng: 77.325981};
@@ -11,4 +14,29 @@ function initMap() {
   //   map: map,
   //   title: 'Atta!'
   // });
+
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+          };
+          console.log(pos);
+          map.setCenter(pos);
+          map.setZoom(16);
+      }, function() {
+          handleLocationError(true, map.getCenter());
+      });
+  } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, map.getCenter());
+  }
+
+}
+//initMap close
+
+function handleLocationError(browserHasGeolocation, pos) {
+    // infoWindow.setContent(browserHasGeolocation ?
+    //     'Error: The Geolocation service failed.' :
+    //     'Error: Your browser doesn\'t support geolocation.');
 }
