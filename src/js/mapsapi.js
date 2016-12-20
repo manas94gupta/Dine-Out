e2b64f5066c924fc780e89598a39da6105acc222
Manas <manas94gupta@gmail.com>
2016-12-20 16:52:29 +0530

    Add a button on map to switch between map styles and styled marker icon.
    Add a custom map style for dark mode and create a custom marker icon using
    svg path data.

--- src/js/mapsapi.js
+++ src/js/mapsapi.js
@@ -7,6 +7,17 @@ function initMap() {
     zoom: 6
     });
 
+    // If this is not set then dark mode button takes two clicks initially
+    // to change style
+    map.setOptions({ styles: '' });
+
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(centerControlDiv);
+
     // This is autocomplete for use in the location search.
     var locationAutocomplete = new google.maps.places.Autocomplete(document.getElementById('location'));
     locationAutocomplete.bindTo('bounds', map);
@@ -33,9 +44,50 @@ function initMap() {
       // If browser doesn't support Geolocation
       handleLocationError(false, map.getCenter());
     }
-
-}
 //initMap close
+}
+
+// Add a button on map to toggle map styles
+function CenterControl(controlDiv, map) {
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#252539';
+    // controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '10px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '10px';
+    controlUI.style.marginLeft = '5px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Toggle Map Mode';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = '#bec0c4';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '10px';
+    controlText.style.paddingRight = '10px';
+    controlText.innerHTML = 'Dark Mode';
+    controlUI.appendChild(controlText);
+
+    // Clicking on the toggle mode button will change the map style
+    controlUI.addEventListener('click', function() {
+        if (map.styles == '') {
+            map.setOptions({ styles: mapDarkStyle });
+            controlText.innerHTML = 'Normal Mode';
+            controlUI.style.backgroundColor = '#d2d4d8';
+            controlText.style.color = '#434343';
+        } else {
+            map.setOptions({ styles: '' });
+            controlText.innerHTML = 'Dark Mode';
+            controlUI.style.backgroundColor = '#252539';
+            controlText.style.color = '#bec0c4';
+        }
+    });
+}
 
 // This will handle any error in detection geoloaction
 function handleLocationError(browserHasGeolocation, pos) {
