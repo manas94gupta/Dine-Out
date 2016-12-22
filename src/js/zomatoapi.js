bc243b6600a1f874dd6356394d32e1621f9ecfd5
Manas <manas94gupta@gmail.com>
2016-12-22 14:16:59 +0530

    Remove extra code from zomatoapi.js

--- src/js/zomatoapi.js
+++ src/js/zomatoapi.js
@@ -1,24 +1,5 @@
 var API_KEY = '8807504cbe0f07822eb741817f606d60';
 
-// $.ajax({
-//     // method: GET,
-//     url: 'https://developers.zomato.com/api/v2.1/search',
-//     dataType: 'json',
-//     headers: {
-//         'Accept': 'application/json',
-//         'user-key': API_KEY
-//     },
-//     data: {
-//         'q': 'Delhi'
-//     },
-//     error: function (xhr) {
-//         console.log(xhr.statusText);
-//     },
-//     success: function (response) {
-//         console.log(response);
-//     }
-// });
-
 function searchZomato(pos) {
     var categoryId;
     if (!viewModel.selectedCategory()) {
