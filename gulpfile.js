7a2623689c1eb71938ac436941939d129376c049
Manas <manas94gupta@gmail.com>
2016-12-25 13:07:04 +0530

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('useref', function(){
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
